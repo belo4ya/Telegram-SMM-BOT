4099db5cc6503b10219a2ef5ee479a5933b58843
Alexey Kovalev <41exey.kov41ev@gmail.com>
2020-08-11 17:30:19 +0300

    ignore

--- test.py
+++
@@ -1,7 +0,0 @@
-header_template = '<b>{}</b>\n' \
-                  '<code>Адресаты</code>: {}\n' \
-                  '<code>Количество</code>: {}\n' \
-                  '<code>Интервал</code>: {}\n' \
-                  '<code>Время выполнения</code>: {}'
-
-print(header_template.format('-', '-', '-', '-', '-'))
